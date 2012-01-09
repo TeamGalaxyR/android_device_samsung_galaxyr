bbe8efb17704a1bb89996392d1c389edd004759f
Mathew Says IdareYou <sportcucs@hotmail.com>
2012-01-09 18:53:22 +0800

    Trying from LG Star P990

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,98 +1,4 @@
-# Copyright (C) 2007 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# config.mk
-#
-# Product-specific compile-time definitions.
-#
-
-LOCAL_PATH:= $(call my-dir)
-
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
-USE_CAMERA_STUB := false
-
-TARGET_NO_BOOTLOADER := true
-
-#TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/galaxyr/recovery_kernel
-#TARGET_RECOVERY_INITRC := device/samsung/galaxyr/recovery.rc
-
-#TARGET_BOARD_PLATFORM := msm7k
-#TARGET_ARCH_VARIANT := armv7-a
-TARGET_CPU_ABI := armeabi-v7a
-
-#TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-#TARGET_BOOTLOADER_BOARD_NAME := galaxyr
-
-# assert
-#TARGET_OTA_ASSERT_DEVICE := galaxyr,GT-I9103
-
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-BOARD_FM_DEVICE := bcm4330
-
-# Wifi related defines
-#BOARD_WPA_SUPPLICANT_DRIVER := AWEXT
-#WIFI_DRIVER_MODULE_PATH     := /system/wifi/ar6000.ko
-#WIFI_DRIVER_MODULE_NAME     := ar6000
-
-WITH_JIT := true
-ENABLE_JSC_JIT := true
-
-#TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-#TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true 
-
-#JS_ENGINE := v8
-
-# OpenGL drivers config file path
-#BOARD_EGL_CFG := device/samsung/galaxyr/egl.cfg
-
-# No fallback font by default (space savings)
-#NO_FALLBACK_FONT:=true
-
-#BOARD_GPS_LIBRARIES := libloc_api
-
-#BOARD_USES_QCOM_HARDWARE := true
-#BOARD_USES_QCOM_LIBS := true
-#BOARD_USES_QCOM_GPS := true
-#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := galaxyr
-#BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
-
-#BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
-
-#BOARD_KERNEL_BASE := 0x13600000
-#BOARD_KERNEL_PAGESIZE := 4096
-#TARGET_PROVIDES_LIBAUDIO := true
-
-#BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-
-#BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
-
-# # cat /proc/mtd
-# dev:    size   erasesize  name
-# mtd0: 00480000 00020000 "recovery"
-# mtd1: 00480000 00020000 "boot"
-# mtd2: 00180000 00020000 "splash"
-# mtd3: 00060000 00020000 "misc"
-# mtd4: 02940000 00020000 "cache"
-# mtd5: 0cf80000 00020000 "system"
-# mtd6: 0d020000 00020000 "userdata"
-# mtd7: 00180000 00020000 "persist"
+TARGET_BOOTLOADER_BOARD_NAME := I9103
 
 # BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x00480000
 # BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00780000
@@ -100,16 +6,8 @@ ENABLE_JSC_JIT := true
 # BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0d020000
 # BOARD_FLASH_BLOCK_SIZE := 131072
 
-# BOARD_BML_BOOT := "/dev/block/bml8"
-# BOARD_BML_RECOVERY := "/dev/block/bml9"
-
 TARGET_USERIMAGES_USE_EXT4 := true
 #BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
 #BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 
-#BOARD_RECOVERY_HANDLES_MOUNT := true
-
-# Don't touch this because the R does not have different UI for Recovery
-
-#BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/cooper/recovery/recovery_ui.c
-#BOARD_CUSTOM_GRAPHICS           := ../../../device/samsung/cooper/recovery/graphics.c
+BOARD_VOLD_MAX_PARTITIONS := 12
