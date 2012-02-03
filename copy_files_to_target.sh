e386083a8675990d85950ab0e80eccfb2b287f2c
Mathew Says IdareYou <sportcucs@hotmail.com>
2012-02-03 16:19:10 +0800

    Taken from Dan

---
+++ copy_files_to_target.sh
@@ -0,0 +1,10 @@
+mkdir -p ../../../out/target/product/galaxyr/system/lib
+mkdir -p ../../../out/target/product/galaxyr/obj/lib
+
+cp proprietary/libaudio.so ../../../out/target/product/galaxyr/system/lib
+cp proprietary/libaudio.so ../../../out/target/product/galaxyr/obj/lib
+cp proprietary/libcamera.so ../../../out/target/product/galaxyr/system/lib
+cp proprietary/libcamera.so ../../../out/target/product/galaxyr/obj/lib
+cp proprietary/libaudiopolicy.so ../../../out/target/product/galaxyr/system/lib
+cp proprietary/libaudiopolicy.so ../../../out/target/product/galaxyr/obj/lib 
+cp kernel ../../../out/target/product/galaxyr/
