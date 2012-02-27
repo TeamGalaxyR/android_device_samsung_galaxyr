8b0760a3eb3f652d44059a130c316109facc6dc8
Mathew Says IdareYou <sportcucs@hotmail.com>
2012-02-27 21:11:14 +0800

    Taken from Dan

--- galaxyr.mk
+++ galaxyr.mk
@@ -3,23 +3,31 @@
 # build for sapphire hardware. This cleanly combines a set of
 # device-specific aspects (drivers) with a device-agnostic
 # product configuration (apps).
-#
 
-# Inherit from those products. Most specific first.
+## Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 #DEVICE_PACKAGE_OVERLAYS := device/samsung/cooper/overlay
+
+# proprietary side of the device
+#$(call inherit-product-if-exists, vendor/samsung/galaxyr/device-vendor.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := galaxyr
 PRODUCT_DEVICE := galaxyr
 PRODUCT_MODEL := GT-I9103
 
-# proprietary side of the device
-#$(call inherit-product-if-exists, vendor/samsung/galaxyr/device-vendor.mk)
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/samsung/galaxyr/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
-PRODUCT_PACKAGES := \
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_PACKAGES += \
     LiveWallpapers \
     LiveWallpapersPicker \
     VisualizationWallpapers \
@@ -41,94 +49,54 @@ PRODUCT_PACKAGES := \
     lights.tegra \
     gralloc.tegra \
     #setup_fs \
-    dexpreopt \
-    Phone \
-    Contacts \
-    SystemUI \
-    framework-res \
-    Settings \
-    Message \
-    Browser \
-    AccountsAndSyncSettings \
-    Bluetooth \
-    Calculator \
-    Calendar \
-    Camera \
-    Email \
-    Launcher2 \
-    HTMLViewer \
-    Mms \
-    Music \
-    PackageInstaller \
-    Protips \
-    Provision \
-    SoundRecorder \
-    Stk \
-    Tag \
-    LatinIME \
-    OpenWnn \
-    PinyinIME \
-    ApplicationsProvider \
-    CalendarProvider \
-    ContactsProvider \
-    DownloadProvider \
-    GoogleContactsProvider \
-    MediaProvider \
-    TelephonyProvider \
-    UserDictionaryProvider
+    dexpreopt
 
 # Set true if you want .odex files
 DISABLE_DEXPREOPT := false    
 
-# Prebuilt libraries that are needed to build open-source libraries
-PRODUCT_COPY_FILES := \
+include $(CLEAR_VARS)
+
+# INIT-scripts
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/init.rc:root/init.rc \
+    device/samsung/galaxyr/ueventd.rc:root/ueventd.rc \
+    device/samsung/galaxyr/init.n1.rc:root/init.n1.rc \
+    device/samsung/galaxyr/ueventd.n1.rc:root/ueventd.n1.rc
+
+include $(CLEAR_VARS)
+
+# Audio and Camera
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/proprietary/libcamera.so:obj/lib/libcamera.so \
+    device/samsung/galaxyr/proprietary/libcamera.so:system/lib/libcamera.so \
     device/samsung/galaxyr/proprietary/libaudio.so:obj/lib/libaudio.so \
+    device/samsung/galaxyr/proprietary/libaudio.so:system/lib/libaudio.so \
     device/samsung/galaxyr/proprietary/libaudiopolicy.so:obj/lib/libaudiopolicy.so \
-    device/samsung/galaxyr/proprietary/libcamera.so:obj/lib/libcamera.so
+    device/samsung/galaxyr/proprietary/libaudiopolicy.so:system/lib/libaudiopolicy.so
+
+include $(CLEAR_VARS)
+
+# RIL-stuff
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxyr/proprietary/libril.so:system/lib/libril.so \
+    device/samsung/galaxyr/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
+    device/samsung/galaxyr/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
+    device/samsung/galaxyr/proprietary/apns-conf.xml:system/etc/apns-conf.xml \
+    device/samsung/galaxyr/proprietary/rild:system/bin/rild
+
+#include $(CLEAR_VARS)
 
 # All the blobs necessary for galaxyr
 PRODUCT_COPY_FILES += \
     device/samsung/galaxyr/proprietary/qwerty.kl:system/usr/keylayout/qwerty.kl \
     device/samsung/galaxyr/proprietary/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
     device/samsung/galaxyr/proprietary/vold.fstab:system/etc/vold.fstab \
-    device/samsung/galaxyr/init.n1.rc:root/init.n1.rc \
-    device/samsung/galaxyr/ueventd.n1.rc:root/ueventd.n1.rc \
-    device/samsung/galaxyr/init.n1.rc:init.n1.rc \
-    device/samsung/galaxyr/ueventd.n1.rc:ueventd.n1.rc \
     #device/samsung/galaxyr/proprietary/AudioFilter.csv:system/etc/AudioFilter.csv \
     #device/samsung/galaxyr/proprietary/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
     device/samsung/galaxyr/proprietary/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/samsung/galaxyr/proprietary/libaudio.so:system/lib/libaudio.so \
-    device/samsung/galaxyr/proprietary/libaudiopolicy.so:system/lib/libaudiopolicy.so \
     #device/samsung/galaxyr/proprietary/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
     #device/samsung/galaxyr/proprietary/hostapd:system/bin/hostapd \
     #device/samsung/galaxyr/proprietary/hostapd.conf:system/etc/wifi/hostapd.conf \
-    device/samsung/galaxyr/proprietary/bcm4330_aps.bin:system/etc/wifi/bcm4330_aps.bin \
-    device/samsung/galaxyr/proprietary/bcm4330_mfg.bin:system/etc/wifi/bcm4330_mfg.bin \
-    device/samsung/galaxyr/proprietary/bcm4330_sta.bin:system/etc/wifi/bcm4330_sta.bin \
-    device/samsung/galaxyr/proprietary/bcm4330_mfg.bin_b0:system/etc/wifi/bcm4330_mfg.bin_b0 \
-    device/samsung/galaxyr/proprietary/bcm4330_sta.bin_b0:system/etc/wifi/bcm4330_sta.bin_b0 \
-    #device/samsung/galaxyr/proprietary/otp.bin.z77:system/wifi/ath6k/AR6003/hw2.0/otp.bin.z77 \
-    device/samsung/galaxyr/proprietary/nvddk_audiofx_core.axf:system/etc/firmware/nvddk_audiofx_core.axf \
-    device/samsung/galaxyr/proprietary/nvddk_audiofx_transport.axf:system/etc/firmware/nvddk_audiofx_transport.axf \
-    device/samsung/galaxyr/proprietary/nvmm_aacdec.axf:system/etc/firmware/nvmm_aacdec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_adtsdec.axf:system/etc/firmware/nvmm_adtsdec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_audiomixer.axf:system/etc/firmware/nvmm_audiomixer.axf \
-    device/samsung/galaxyr/proprietary/nvmm_h264dec.axf:system/etc/firmware/nvmm_h264dec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_jpegdec.axf:system/etc/firmware/nvmm_jpegdec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_jpegenc.axf:system/etc/firmware/nvmm_jpegenc.axf \
-    device/samsung/galaxyr/proprietary/nvmm_manager.axf:system/etc/firmware/nvmm_manager.axf \
-    device/samsung/galaxyr/proprietary/nvmm_mp2dec.axf:system/etc/firmware/nvmm_mp2dec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_mp3dec.axf:system/etc/firmware/nvmm_mp3dec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_mpeg4dec.axf:system/etc/firmware/nvmm_mpeg4dec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_reference.axf:system/etc/firmware/nvmm_reference.axf \
-    device/samsung/galaxyr/proprietary/nvmm_service.axf:system/etc/firmware/nvmm_service.axf \
-    device/samsung/galaxyr/proprietary/nvmm_sorensondec.axf:system/etc/firmware/nvmm_sorensondec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_vc1dec.axf:system/etc/firmware/nvmm_vc1dec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_wavdec.axf:system/etc/firmware/nvmm_wavdec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_wmadec.axf:system/etc/firmware/nvmm_wmadec.axf \
-    device/samsung/galaxyr/proprietary/nvmm_wmaprodec.axf:system/etc/firmware/nvmm_wmaprodec.axf \
-    device/samsung/galaxyr/proprietary/nvrm_avp.bin:system/etc/firmware/nvrm_avp.bin \
     device/samsung/galaxyr/proprietary/media_profiles.xml:system/etc/media_profiles.xml
 
 # Install the features available on this device.
@@ -163,17 +131,25 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
     ro.com.android.dateformat=dd-MM-yyyy \
-    ro.ril.hsxpa=3 \
-    ro.ril.gprsclass=10 
-    #ro.media.dec.jpeg.memcap=10000000
+    ro.ril.enable.3g.prefix=1 \
+    ro.ril.enable.a53=1 \
+    ro.ril.enable.dtm=1 \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.gprsclass=12 \
+    ro.ril.hep=1 \
+    ro.ril.hsdpa.category=8 \
+    ro.ril.hsupa.category=5 \
+    ro.ril.hsxpa=2 \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs="-d /dev/ttys0"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.telephony.ril_class=samsung \
-    wifi.interface=wlan0 \
+    wifi.interface=eth0 \
     wifi.supplicant_scan_interval=30 \
     ro.com.android.dataroaming=false \
-    ro.board.platform=tetra
+    ro.board.platform=tegra
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 
@@ -205,3 +181,4 @@ PRODUCT_LOCALES += hdpi
 # commonly available
 
 #$(call inherit-product-if-exists, vendor/samsung/galaxyr/device-vendor-blobs.mk)
+
